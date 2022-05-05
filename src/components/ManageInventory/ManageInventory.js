150926bb57d8ba816f5df0cec9c35b7400bd2db9
PranoySarker <pranoysarker01@gmail.com>
2022-05-06 03:06:25 +0600

    private route implemented

--- src/components/ManageInventory/ManageInventory.js
+++ src/components/ManageInventory/ManageInventory.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const ManageInventory = () => {
+    const [user, loading, error] = useAuthState(auth);
+
     return (
         <div>
             <h2>This is manage inventory</h2>
--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../firebase.init';
@@ -6,6 +7,16 @@ import auth from '../../firebase.init';
 const PrivateRoute = ({ children }) => {
     const [user, loading, error] = useAuthState(auth);
     let location = useLocation();
+
+    if (loading) {
+        return (
+            <div className='text-center mt-5'>
+                <Spinner animation="grow" variant="primary" />
+                <Spinner animation="grow" variant="secondary" />
+                <Spinner animation="grow" variant="success" />
+            </div>
+        );
+    }
 
     if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace />;
