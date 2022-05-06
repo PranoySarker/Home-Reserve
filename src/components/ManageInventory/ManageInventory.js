d74c1874be7f390cb246b3888e45e8a03b3484ee
PranoySarker <pranoysarker01@gmail.com>
2022-05-07 05:15:13 +0600

    delete button implemented

--- src/components/ManageInventory/ManageInventory.js
+++ src/components/ManageInventory/ManageInventory.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { Button, Table } from 'react-bootstrap';
-import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
-import auth from '../../firebase.init';
 import useInventory from '../hooks/useInventory';
 
 const ManageInventory = () => {
     const [inventories, setInventories] = useInventory([]);
     const navigate = useNavigate();
+
+    const handleDelete = id => {
+        const proceed = window.confirm('Are You Sure To Delete?');
+        if (proceed) {
+            const url = `http://localhost:5000/inventory/${id}`;
+            fetch(url, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        const remaining = inventories.filter(inventory => inventory._id !== id)
+                        setInventories(remaining);
+                    }
+                })
+        }
+    }
 
     return (
         <div>
@@ -32,6 +48,7 @@ const ManageInventory = () => {
                                 <td>
                                     <Button variant='primary' onClick={() => navigate(
                                         `/inventory/${inventory._id}`)}>More details</Button>
+                                    <Button className='ms-2' variant='danger' onClick={() => handleDelete(inventory._id)}>Delete</Button>
                                 </td>
                             </tr>
                         )
