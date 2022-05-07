62affbaf1a5e7db1413b357f909b7a9c98338e0f
PranoySarker <pranoysarker01@gmail.com>
2022-05-07 22:41:52 +0600

    update restock

--- src/components/Login/ItemDetails/ItemDetails.js
+++ src/components/Login/ItemDetails/ItemDetails.js
@@ -1,17 +1,45 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const ItemDetails = () => {
     const { id } = useParams();
     const [inventory, setInventory] = useState({});
+    const [isReload, setIsReload] = useState(false);
 
     useEffect(() => {
         const url = `http://localhost:5000/inventory/${id}`;
         fetch(url)
             .then(res => res.json())
             .then(data => setInventory(data))
-    }, [])
+    }, [isReload])
+
+    const handleRestock = event => {
+        event.preventDefault();
+        const quantity = event.target.quantity.value;
+        console.log(quantity);
+
+        const url = `http://localhost:5000/inventory/${id}`;
+        fetch(url, {
+            method: 'PUT',
+            body: JSON.stringify({
+                quantity: quantity
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                event.target.reset();
+                setIsReload(!isReload);
+            })
+    }
+    const handleDelivered = id => {
+
+    }
     return (
         <div>
             <div className='mt-5 d-flex justify-content-center '>
@@ -24,8 +52,19 @@ const ItemDetails = () => {
                     <h4>Company Name: {inventory.supplier}</h4>
                     <h4>Quantity: {inventory.quantity}</h4>
                     <p>{inventory.description}</p>
-                    <Button>Delivered</Button>
+                    <Button onClick={() => handleDelivered(inventory.id)}>Delivered</Button>
                 </div>
+            </div>
+            <div >
+                <Form className='w-25 d-block mx-auto mb-5' onSubmit={handleRestock}>
+                    <h3>Restock Item:</h3>
+                    <Form.Group className="mb-3 " >
+                        <Form.Control type="text" name='quantity' placeholder='Number of product' />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Re-Stock
+                    </Button>
+                </Form>
             </div>
             <div>
 
